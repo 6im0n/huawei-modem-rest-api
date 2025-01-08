c0b1ba7d3f09d56ecc3410088e446c1b3c341d85
6im0n <simon.gl3135@orange.fr>
2025-01-08 01:06:26 +0100

    :construction: Work on sms, sending sms working

--- src/sms/sms.controller.ts
+++ src/sms/sms.controller.ts
@@ -1,15 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller} from '@nestjs/common';
 import { Get, Post } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common';
 import { ApiKeyGuard } from '../api-key/api-key.guard';
+import { SmsService } from './sms.service';
+
 
 @Controller('sms')
 export class SmsController {
-  constructor() {}
+  constructor(private readonly smsService: SmsService ) {}
 
   @Get('info')
   @UseGuards(ApiKeyGuard)
-  getInfo(): { message: string } {
-    return { message: 'this is the sms module' };
+  getInfo() {
+    return this.smsService.about();
   }
+
+  @Post('send')
+  @UseGuards(ApiKeyGuard)
+  sendSms(@Body('phoneNumber') phoneNumber: string, @Body('message') message: string) {
+    return this.smsService.sendSms(phoneNumber, message);
+  }
+
 }
--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Connection, Sms } from 'huawei-lte-api';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = new Connection(process.env.HUAWEI_CONNECTION, 1000);
 
 @Injectable()
 export class SmsService {
@@ -6,7 +12,10 @@ export class SmsService {
     return 'This is a sms service';
   }
   sendSms(phoneNumber: string, message: string) {
+    const sms = new Sms(connection);
+    sms.sendSms([phoneNumber], message).then(r => {
+      console.log(r);
+    });
     return `Sending sms to ${phoneNumber} with message: ${message}`;
   }
-
 }
